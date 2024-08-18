4a8d9d824596bc2f653228468f90aa5594223bd9
Ubuntu <ubuntu@ip-172-31-40-103.us-east-2.compute.internal>
2024-08-18 19:31:14 +0000

    portfolio

---
+++ portfolio/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <Header />
+    <About />
+    <Skills />
+    <Portfolio />
+    <Contact />
+    <Footer />
+</template>
+
+<script>
+import Header from '@/pages/Header.vue'
+import About from '@/pages/About.vue'
+import Skills from '@/pages/Skills.vue'
+import Portfolio from '@/pages/Portfolio.vue'
+import Contact from '@/pages/Contact.vue'
+import Footer from '@/pages/Footer.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Header, About, Skills, Portfolio, Contact, Footer
+    },
+}
+
+</script>
+
+<style>
+@import "./styles/css/lol.css"
+</style>
---
+++ portfolio/src/old.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// App is rendered into the root DOM node with id=app
+// (see public/index.html element with id=app)
+createApp(App).mount('#app')
